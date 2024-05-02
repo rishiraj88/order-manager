c75d5efcc112c708b7c17b0a0256a6c9df68f8fa
Rishi Raj <rishiraj88+github@gmail.com>
2024-05-02 19:10:26 +0200

    Add Inventory client (a Feign client) to OrderService to query stock for order placement.

--- order/src/main/java/om/order/svc/OrderServiceImpl.java
+++ order/src/main/java/om/order/svc/OrderServiceImpl.java
@@ -1,6 +1,7 @@
 package om.order.svc;
 
 import lombok.RequiredArgsConstructor;
+import om.order.clients.InventoryClient;
 import om.order.dao.OrderRepo;
 import om.order.dto.OrderReq;
 import om.order.entity.Order;
@@ -12,16 +13,20 @@ import java.util.UUID;
 @Service @RequiredArgsConstructor
 public class OrderServiceImpl implements IOrderService {
     private final OrderRepo  orderRepo;
+    private final InventoryClient inventoryClient;
     @Override
     public void createOrder(OrderReq orderReq) {
-        Order newOrder = Order.builder()
-                .id(UUID.randomUUID().toString())
-                .orderNumber(orderReq.orderNumber())
-                .itemSkuCode(orderReq.itemSkuCode())
-                .pricePerItem(orderReq.pricePerItem())
-                .quantity(orderReq.quantity())
-                .build();
-orderRepo.save(newOrder);
-
+        if(inventoryClient.isInStock(orderReq.itemSkuCode(),orderReq.quantity())) {
+            Order newOrder = Order.builder()
+                    .id(UUID.randomUUID().toString())
+                    .orderNumber(orderReq.orderNumber())
+                    .itemSkuCode(orderReq.itemSkuCode())
+                    .pricePerItem(orderReq.pricePerItem())
+                    .quantity(orderReq.quantity())
+                    .build();
+            orderRepo.save(newOrder);
+        } else {
+            throw new InventoryShortOfStockException(orderReq.itemSkuCode(), orderReq.quantity());
+        }
     }
 }
