65d56f92f4631085eadef78fcfb3715ba9b69dfa
Rishi Raj <rishiraj88+github@gmail.com>
2024-05-06 21:57:32 +0200

    One step towards cleanup.

--- order/src/main/java/om/order/clients/InventoryClient.java
+++ order/src/main/java/om/order/clients/InventoryClient.java
@@ -2,7 +2,6 @@ package om.order.clients;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(value="inventory", url="${inventory.connect.url}")
