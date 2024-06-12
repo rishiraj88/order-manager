8d466ee50043e644e6a9208caea80be2abda334f
Rishi Raj <rishiraj88+github@gmail.com>
2024-06-12 10:49:16 +0200

    Add rate limiter.

--- order/src/main/java/om/order/clients/InventoryClient.java
+++ order/src/main/java/om/order/clients/InventoryClient.java
@@ -1,11 +1,25 @@
 package om.order.clients;
 
-import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
+import groovy.util.logging.Slf4j;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.service.annotation.GetExchange;
 
+@Slf4j
 public interface InventoryClient {
+
+    Logger log = LoggerFactory.getLogger(InventoryClient.class);
+
 @GetExchange("/api/inventory")
+@CircuitBreaker(name="inventory",fallbackMethod = "fallbackMethod")
+@Retry(name="inventory")
 boolean isInStock(@RequestParam String skuCode, @RequestParam Integer quantityForQuery);
+
+default boolean fallbackMethod(String skuCode, Integer quantityForQuery, Throwable throwable) {
+    log.info("Sufficient quantity of SKU {} not found in inventory. Reason: {}",skuCode,throwable.getMessage());
+    return false;
+}
 }
