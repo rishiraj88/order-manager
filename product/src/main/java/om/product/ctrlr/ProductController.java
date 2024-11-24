327583f324e70a43d0e2c31263f33a6ba0e588e0
Rishi Raj <rishiraj88+github@gmail.com>
2024-11-24 11:11:14 +0100

    Showcase two approaches to add Status Code to Response.

--- product/src/main/java/om/product/ctrlr/ProductController.java
+++ product/src/main/java/om/product/ctrlr/ProductController.java
@@ -1,10 +1,12 @@
 package om.product.ctrlr;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import om.product.dto.ProductReq;
 import om.product.dto.ProductResp;
 import om.product.svc.IProductService;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,31 +18,33 @@ import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/api/products") //TODO add API versioning
+@Slf4j
+@RequestMapping("/api/V1/products") //TODO add API versioning
 public class ProductController {
     private final IProductService productService;
 
     @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
+    @ResponseStatus(HttpStatus.CREATED) // Smarter way to add Status Code to Response
     public ProductResp addProduct(@RequestBody ProductReq productReq) {
         try {
             Thread.sleep(6000);
         } catch (InterruptedException e) {
+            log.debug(e.getMessage());
             throw new RuntimeException(e);
         }
         return productService.addProduct(productReq);
     }
 
     @GetMapping
-    @ResponseStatus(HttpStatus.OK)
-    public List<ProductResp> getAllProducts() {
-    /*
-    //uncomment this code fragment to test timeout and retry with Resilience4j
+    public ResponseEntity<List<ProductResp>> getAllProducts() {
+    /* for development and debugging
+    //Dev Tools: Uncomment this code fragment to test timeout and retry with Resilience4j tooling
         try {
             Thread.sleep(5000);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }*/
-        return productService.getAllProducts();
+        // Traditional way to add Status Code Response
+        return ResponseEntity.ok(productService.getAllProducts());
     }
 }
