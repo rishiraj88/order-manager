243ce0ef1950d45f4e43916d75300170e3d4d31d
Rishi Raj <rishiraj88+github@gmail.com>
2024-12-23 20:51:13 +0100

    Add a comment to signify the need for one more service: Cart.

--- order/src/main/java/om/order/svc/IOrderService.java
+++ order/src/main/java/om/order/svc/IOrderService.java
@@ -2,6 +2,17 @@ package om.order.svc;
 
 import om.order.dto.OrderReq;
 
-public interface IOrderService { public void createOrder(OrderReq orderReq);
+public interface IOrderService {
 
+    public void createOrder(OrderReq orderReq);
+
+    /*
+    ** To check items from shopping card out **
+    * 1. View the list of items I am buying.
+    * 2. Enter my billing address and shipping address.
+    * 3. View the total amount (price in gross) due for payment.
+    * 4. Confirm and proceed to checkout stage.
+    * 5. A notification email should be received by me (customer).
+     */
+    //checkout functionality may be added separately in Cart microservice
 }
